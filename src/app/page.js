bfc2ba9a54445a2f7a4ee881093a7cc1a000f8f8
dhanush <dhanushpk50@gmail.com>
2024-07-20 10:53:13 +0530

    assets and components directory added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/Hero/Hero";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main>
+      <Navbar />
+      <Hero />
     </main>
   );
 }
