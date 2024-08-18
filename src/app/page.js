201cffc597b66a9ce9f626076b6250f0fb80c1bd
dhanush <dhanushpk50@gmail.com>
2024-08-18 12:37:11 +0530

    change in bg

--- src/app/page.js
+++ src/app/page.js
@@ -25,22 +25,26 @@ export default function Home() {
     window.scrollTo(0, 0);
 
     const sections = gsap.utils.toArray("section");
-    const isMobile = window.innerWidth < 768;
+    const calculateEndValue = () => {
+      const heroElement = document.getElementById("hero");
+      if (!heroElement) return;
 
-    const totalHeight = sections.reduce((acc, section) => acc + section.offsetHeight, 0);
-    const viewportHeight = window.innerHeight;
-    const extraPixels = isMobile ? 130 : 0.2 * viewportHeight;
+      const heroTop = heroElement.getBoundingClientRect().top + window.scrollY;
+      const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-    const snapPoints = sections.map((section) => (section.offsetTop - extraPixels) / totalHeight);
-
+      return isMobile
+        ? heroTop - 100 
+        : heroTop - window.innerHeight * 0.2; 
+    };
+    
     gsap.to(sections, {
       scrollTrigger: {
         trigger: scrollRef.current,
         start: "top top",
-        end: "bottom bottom",
+        end: calculateEndValue,
         scrub: 1,
         snap: {
-          snapTo: snapPoints,
+          snapTo: 1,
           duration: { min: 0.2, max: 0.8 },
           ease: "back.out(1)",
         },
@@ -60,32 +64,26 @@ export default function Home() {
   };
 
   return (
-    <main ref={scrollRef} className="bg-opacity-0 bg-contain bg-repeat-y">
+    <main className="bg-opacity-0 bg-contain bg-repeat-y">
+      <div className="snapping" ref={scrollRef} >
       <Navbar />
-      <section>
-      <div className="bg-white bg-pattern bg-fixed h-screen w-screen flex items-center justify-center">
-          <a onClick={handleScrollToHero} className="absolute bottom-0 mb-10">
-            <Image src={arrowUp} alt="arrow up" className="w-10 h-10 md:w-16 md:h-16" />
-          </a>
-        </div>
-      </section>
-      <section id="hero">
-        <Hero />
-      </section>
-      <section id="about">
-        <CountDownTimer />
-        <About />
-      </section>
-      <section id="snapshot">
-        <Statusbar />
-        <Snapshots />
-      </section>
-      <section id="speakers">
-        <Speakers />
-      </section>
-      <section id="journey">
-        <OurJourney />
-      </section>
+        <section>
+          <div className="bg-white bg-pattern bg-fixed h-screen w-screen flex items-center justify-center">
+            <a onClick={handleScrollToHero} className="absolute bottom-0 mb-10">
+              <Image src={arrowUp} alt="arrow up" className="w-10 h-10 md:w-16 md:h-16" />
+            </a>
+          </div>
+        </section>
+        <section id="hero">
+          <Hero />
+        </section>
+      </div>
+      <CountDownTimer />
+      <About />
+      <Statusbar />
+      <Snapshots />
+      <Speakers />
+      <OurJourney />
       <Footer />
     </main>
   );
