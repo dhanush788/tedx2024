393d829f14ed60dece026759d5fae6fb11096e90
dhanush <dhanushpk50@gmail.com>
2024-08-24 13:29:05 +0530

    content added

--- src/app/ticket/page.js
+++ src/app/ticket/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex text-tedRed font-thunder font-bold items-center justify-center text-4xl md:text-9xl w-full h-screen'>COMMING SOON</div>
+    <div className='flex text-tedRed font-thunder font-bold items-center justify-center text-4xl md:text-9xl w-full h-screen'>COMING SOON</div>
   )
 }
 
