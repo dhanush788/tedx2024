42acd7b12f89dc30a0ef9f96368f8f381df43399
dhanush <dhanushpk50@gmail.com>
2024-09-01 12:26:01 +0530

    5th year

--- src/app/page.js
+++ src/app/page.js
@@ -127,7 +127,7 @@ export default function Home() {
           <div className="bg-white bg-pattern bg-fixed h-screen w-screen flex items-center justify-center transition-all duration-1000">
             <div ref={elementRef}>
               <div
-                className="flex gap-2 absolute top-[53%] md:top-[53%] opacity-0 animate-fadeIn left-[50%] -translate-x-[40%] md:-translate-x-1/2 w-full md:w-auto"
+                className="flex gap-2 absolute top-[53%] md:top-[53%] opacity-0 animate-fadeIn left-[50%] -translate-x-[47%] md:-translate-x-1/2 w-full md:w-auto"
               >
                 <>
                   {[...Array(2)].map((_, index) => (
