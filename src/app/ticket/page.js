1e50508829f81ed59c95f6867a0831e6a2f37d42
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-09-03 07:35:53 +0530

    added ticketcard animation and popup
    Signed-off-by: Roopesh O R <62459820+Roopesh2@users.noreply.github.com>

--- src/app/ticket/page.js
+++ src/app/ticket/page.js
@@ -1,22 +1,41 @@
 "use client"
-import ContactUs from '@/components/ContactUs/ContactUs';
-import Copyright from '@/components/Copyright/Copyright';
 import FAQSection from '@/components/FAQSection/FAQSection';
+import Footer from '@/components/Footer/Footer';
 import Marquee from '@/components/Marquee/Marquee';
 import Sponsors from '@/components/Sponsors/Sponsors';
 import TicketNavbar from '@/components/TicketNavbar/TicketNavbar';
+import { useState } from 'react';
+
+const REG_POPUP_MSG = ` always felt like I could do anything. That’s the main
+thing people are controlled by! Thoughts- their perception
+of themselves! They're slowed down by their perception of
+themselves. If you're taught you can’t do anything, you
+won’t do anything. I was taught I could do everything.`
+const CUSAT_POPUP_MSG = ` always felt like I could do anything. That’s the main
+thing people are controlled by! Thoughts- their perception
+of themselves! They're slowed down by their perception of
+themselves. If you're taught you can’t do anything, you
+won’t do anything. I was taught I could do everything.`
 
 const TicketCard = ({
 	type,
 	price,
-	redirect
+	redirect,
+	popupHandler,
+	popupMessageSetter,
+	popupTitleSetter,
 }) => {
-	return <div className='text-left w-[90vw] md:w-[30vw] border-black border rounded-[8px] px-4 py-7'>
+	function showPopup() {
+		popupHandler(true);
+		popupMessageSetter();
+		popupTitleSetter(type);
+	}
+	return <div className='text-left w-[90vw] md:w-[35vw] border-black border rounded-[8px] px-4 py-7 hover:scale-105 transform duration-75 overflow-hidden'>
 		<p className='text-3xl text-tedRed text-left mb-10'>{type}</p>
 		<p className='text-2xl mt-12'>₹{price}</p>
 
 		<a href={redirect}>
-			<button className="flex bg-black text-white p-3 pt-4 rounded-[8px] gap-3 items-center mt-2">
+			<button className="flex bg-black border border-black text-white p-3 pt-4 rounded-[8px] gap-3 items-center mt-2 hover:bg-tedRed transform duration-75">
 				<span className="text-xl leading-5">BUY TICKETS</span>
 				<span className="btn__svg">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.85 24.85"
@@ -30,16 +49,18 @@ const TicketCard = ({
 			</button>
 		</a>
 
-		<p className='text-md mt-5 inline-flex gap-2'>
+		<p className='text-md mt-5 inline-flex gap-2 cursor-pointer border border-black p-1 rounded' onClick={showPopup}>
 			<span>
 				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" /></svg>
 			</span>
 			<span>DETAILS</span>
 		</p>
-
 	</div>
 }
 const page = () => {
+	const [showPopup, setShowPopup] = useState(false);
+	const [popupTitle, setPopupTitle] = useState("");
+	const [popupMessage, setPopupMessage] = useState("");
 	return (<>
 		<TicketNavbar />
 		<div className='mt-[18vh]'></div>
@@ -49,17 +70,58 @@ const page = () => {
 			<Sponsors />
 			<div className='get-tickets mt-[10vh]'>
 				<p className='md:text-6xl text-5xl text-center md:text-left leading-none'>GET YOUR<br /><span className='text-tedRed'>TICKETS</span> NOW</p>
-				<div className='ticket-card-container flex flex-col items-center gap-5 md:flex-row justify-around p-10'>
-					<TicketCard redirect="/ticket/book?type=regular" price="1000" type="REGULAR" />
-					<TicketCard redirect="/ticket/book?type=cusatian" price="800" type="CUSAT STUDENTS" />
+				<div className='ticket-card-container flex flex-col items-center gap-10 md:flex-row justify-start p-5'>
+					<TicketCard popupTitleSetter={setPopupTitle} popupMessageSetter={() => setPopupMessage(REG_POPUP_MSG)} popupHandler={setShowPopup} redirect="/ticket/book?type=regular" price="1000" type="REGULAR" />
+					<TicketCard popupTitleSetter={setPopupTitle} popupMessageSetter={() => setPopupMessage(CUSAT_POPUP_MSG)} popupHandler={setShowPopup} redirect="/ticket/book?type=cusatian" price="800" type="CUSAT STUDENTS" />
 				</div>
 			</div>
 			<FAQSection />
-			<footer className='flex flex-col gap-5 py-5'>
-				<ContactUs />
-			</footer>
-			<Copyright />
 		</main>
+		<Footer />
+		{showPopup ? (
+			<div className="absolute top-0 left-0 z-[9999] w-[100vw] h-[100vw]">
+				<div
+					className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none max-md:m-10"
+				>
+					<div className="relative w-auto my-6 mx-auto max-w-3xl">
+						{/*content*/}
+						<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+							{/*header*/}
+							<div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+								<h3 className="text-4xl font-thunder ">
+									{popupTitle}
+								</h3>
+								<button
+									className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+									onClick={() => setShowPopup(false)}
+								>
+									<span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+										×
+									</span>
+								</button>
+							</div>
+							{/*body*/}
+							<div className="relative p-6 flex-auto">
+								<p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+									{popupMessage}
+								</p>
+							</div>
+							{/*footer*/}
+							<div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+								<button
+									className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+									type="button"
+									onClick={() => setShowPopup(false)}
+								>
+									Close
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+			</div>
+		) : null}
 	</>
 	)
 }
