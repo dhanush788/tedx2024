d7f4ebe05e176c9c786a6c44bdf2eb03f4980560
dhanush <dhanushpk50@gmail.com>
2024-09-14 20:31:44 +0530

    sponsor added

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import infopark from "@/assets/img/sponsers/infoparkLogo.svg";
 const Sponsors = () => {
 	return <>
-		<p className='font-thunder font-bold md:text-6xl text-5xl leading-snug text-center'><span className='text-tedRed'>OUR</span> SPONSORS</p>
+		<p className='font-thunder font-bold md:text-6xl text-5xl leading-snug text-center'><span className='text-tedRed'>TITLE</span> SPONSOR</p>
 		<div className="flex flex-col gap-5 sm:flex-row justify-around mt-5">
 			<div className="flex flex-col  items-center">
-				<p className="font-thunder font-bold text-3xl md:text-5xl"><span className="text-tedRed">TITLE</span> SPONSOR</p>
+				{/* <p className="font-thunder font-bold text-3xl md:text-5xl"><span className="text-tedRed">TITLE</span> SPONSOR</p> */}
 				<Image src={infopark} className="w-[50vw] sm:w-[25vw] h-auto" />
 			</div>
 			{/* <div className="flex flex-col items-center">
