ad1f16c80a54b85fe394b09e33409fc5fb28bbbe
dhanush <dhanushpk50@gmail.com>
2024-09-20 13:15:18 +0530

    ticket released

--- src/app/ticket/success/page.js
+++ src/app/ticket/success/page.js
@@ -12,7 +12,7 @@ const Page = () => {
           Your ticket has been booked successfully
         </p>
         <p className='text-base md:text-xl md:pt-5'>
-          You've successfully booked your ticket for <span className='font-bold'><span className='text-tedRed'>TED<sup>x</sup></span>CUSAT24</span> You will receive an email with the details of your booking within 1 - 2 business days. If you have any questions or need further assistance, feel free to contact us at <a href='mailto:outreach@tedxcusat.in' className='underline font-bold'>outreach@tedxcusat.in</a>
+          You've successfully booked your ticket for <span className='font-bold'><span className='text-tedRed'>TED<sup>x</sup></span>CUSAT24</span> You will receive an email with the details of your booking 2 to 3 days before the event. If you have any questions or need further assistance, feel free to contact us at <a href='mailto:outreach@tedxcusat.in' className='underline font-bold'>outreach@tedxcusat.in</a>
           <br />
         </p>
         <p className='text-sm md:text-lg pt-5 md:pt-8'>
