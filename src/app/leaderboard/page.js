ced4131f2f6b625dd2432a7c3dfc5b5c3fbb09c6
dhanush <dhanushpk50@gmail.com>
2024-09-21 15:36:15 +0530

    leaderboard added

---
+++ src/app/leaderboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Leaderboard from './leaderboard'
+
+const page = () => {
+  return (
+    <div>
+        <Leaderboard />
+    </div>
+  )
+}
+
+export default page
