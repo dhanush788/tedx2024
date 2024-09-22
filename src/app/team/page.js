c51ada9a72ec472481015ffa6779dac79c73a584
dhanush <dhanushpk50@gmail.com>
2024-09-22 10:57:50 +0530

    team updated

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -39,7 +39,7 @@ const data = [
         img: sheena
     },
     {
-        heading: "A S Adil Mohammad",
+        heading: "Adil Mohammad",
         subHeading: "Curator",
         img: adil
     },
@@ -98,16 +98,16 @@ const data = [
 
 const Cards = ({ item, i }) => {
     return (
-        <div key={i} className={`flex flex-col min-h-[350px] md:min-h-[350px] last:mr-2`}>
-            <div className='border border-black rounded-[6px] md:rounded-15 flex flex-col relative min-w-[240px] md:min-w-[300px] '>
+        <div key={i} className={`flex flex-col min-h-[200px] md:min-h-[350px] last:mr-2`}>
+            <div className='border border-black rounded-[6px] md:rounded-15 flex flex-col relative min-w-[140px] md:min-w-[300px] '>
                 <div className='overflow-hidden w-full h-full rounded-[6px] md:rounded-15'>
-                    <Image src={item.img} alt="" id='image' className='h-[350px] w-full object-cover ease-in-out object-top hover:scale-105 transition-all duration-300 ease' />
+                    <Image src={item.img} alt="" id='image' className='h-[200px] md:h-[350px] w-full object-cover ease-in-out object-top hover:scale-105 transition-all duration-300 ease' />
                 </div>
-                <div className={`px-4 absolute rounded-[6px] md:rounded-15 bottom-0 left-0 w-full flex flex-col transition-all duration-300 ease bg-white min-h-[75px] md:min-h-[100px]`}>
-                    <p className={`font-Geist font-bold text-xl md:text-2xl pt-2 md:pt-4 uppercase`}>{item.heading}</p>
-                    <p className='font-Geist text-base md:text-lg text-[#EB0028]'>{item.subHeading}</p>
+                <div className={`px-4 absolute rounded-[6px] md:rounded-15 bottom-0 left-0 w-full flex flex-col transition-all duration-300 ease bg-white min-h-[55px] md:min-h-[100px]`}>
+                    <p className={`font-Geist font-bold text-[15px] md:text-2xl pt-2 md:pt-4 uppercase`}>{item.heading}</p>
+                    <p className='font-Geist text-sm md:text-lg text-[#EB0028]'>{item.subHeading}</p>
                 </div>
-                <div className='min-h-[75px] md:min-h-[100px]'></div>
+                <div className='min-h-[55px] md:min-h-[100px]'></div>
             </div>
         </div>
     )
@@ -123,7 +123,7 @@ const Team = () => {
             <div className='flex flex-row justify-between px-[4%] md:px-[8%] py-5 mt-5'>
                 <p className='font-thunder font-bold md:text-6xl text-4xl leading-snug max-w-md text-left '>MEET OUR <span className='text-[#EB0028]'>TEAM</span> </p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-4 overflow-scroll md:overflow-hidden w-full h-full px-[4%] md:px-[8%] gap-3 md:gap-6 pb-6'>
+            <div className='grid grid-cols-2 md:grid-cols-4 overflow-scroll md:overflow-hidden w-full h-full px-[4%] md:px-[8%] gap-3 md:gap-6 pb-6'>
                 {data.map((item, i) => (
                     <Cards item={item} key={i} />
                 ))
