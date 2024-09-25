42b621889235674cfc7d55e9ac32ff88f0cd509e
dhanush <dhanushpk50@gmail.com>
2024-09-26 00:08:51 +0530

    leaderboard

--- src/app/leaderboard/leaderboard.js
+++ src/app/leaderboard/leaderboard.js
@@ -9,7 +9,7 @@ const Leaderboard = () => {
 
   // Load CSV file and parse it
   useEffect(() => {
-    Papa.parse('/leaderboard.csv', {
+    Papa.parse('/leaderboard1.csv', {
       download: true,
       header: true,
       complete: (result) => {
@@ -17,6 +17,7 @@ const Leaderboard = () => {
         const top = data.slice(0, 3); // Top 3 contributors
         setTopContributors(top);
         setAllContributors(data);
+        console.log(data)
       },
     });
   }, []);
@@ -33,7 +34,7 @@ const Leaderboard = () => {
           {topContributors.map((contributor, index) => (
             <div className={`contributor-card rank-${index + 1}`} key={index}>
               <span className="rank">{contributor.Rank < 10 ? `0${contributor.Rank}` : contributor.Rank}</span>
-              <span className="name">{contributor.Name}</span>
+              <span className="name">{contributor.NAME}</span>
               <span className="score">{contributor.Score}</span>
             </div>
           ))}
@@ -53,7 +54,7 @@ const Leaderboard = () => {
           {allContributors.map((contributor, index) => (
             <React.Fragment key={index}>
               <div className="cell">{index+1}</div>
-              <div className="cell">{contributor.Name}</div>
+              <div className="cell">{contributor.NAME}</div>
               <div className="cell">{contributor.Score}</div>
             </React.Fragment>
           ))}
