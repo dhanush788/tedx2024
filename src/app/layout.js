10a70102b3cb4b04612c105635c90f65112a6c91
Abdulrayifvp <rayifvp336@gmail.com>
2024-12-24 10:35:26 +0530

    microsoft clarity integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -72,6 +72,15 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
         {children}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
         </body>
     </html>
   );
